f5f4a85db4808ae386cfba765aada8619572a706
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-17 23:40:23 +0530

    network: align endpoint request and response contracts

--- rebase-ios/Core/Networking/APIRequest.swift
+++ rebase-ios/Core/Networking/APIRequest.swift
@@ -1,12 +1,43 @@
 import Foundation
 
+enum RequestBody {
+    case json(AnyEncodable)
+    case multipart(data: Data, contentType: String)
+}
+
 struct APIRequest<Response: Decodable> {
     let path: String
     var method: HTTPMethod = .get
     var queryItems: [URLQueryItem] = []
     var headers: [String: String] = [:]
-    var body: AnyEncodable?
+    var body: AnyEncodable? {
+        get {
+            if case .json(let enc) = requestBody { return enc }
+            return nil
+        }
+        set {
+            if let enc = newValue { requestBody = .json(enc) }
+            else { requestBody = nil }
+        }
+    }
+    var requestBody: RequestBody?
     var requiresAuth: Bool = true
+
+    init(
+        path: String,
+        method: HTTPMethod = .get,
+        queryItems: [URLQueryItem] = [],
+        headers: [String: String] = [:],
+        body: AnyEncodable? = nil,
+        requiresAuth: Bool = true
+    ) {
+        self.path = path
+        self.method = method
+        self.queryItems = queryItems
+        self.headers = headers
+        self.requestBody = body.map { .json($0) }
+        self.requiresAuth = requiresAuth
+    }
 }
 
 struct AnyEncodable: Encodable {
